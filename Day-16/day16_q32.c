08bfeefd34598edc01c884fddbc7a6efeebff6da
Shreyansh Yadav <89652665+SHREYANSH-codr@users.noreply.github.com>
2025-11-09 02:24:06 +0530

    Add files via upload

---
+++ Day-16/day16_q32.c
@@ -0,0 +1,26 @@
+//Q32: Write a program to check if a number is a palindrome.
+
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, original;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    if (original == reversed) {
+        printf("Palindrome\n");
+    } else {
+        printf("Not palindrome\n");
+    }
+
+    return 0;
+}
